8cc38ded7ff3d45cab69c260ef8fc64a430445aa
Christian <chris113377@gmail.com>
2020-01-29 12:39:56 -0600

    Updates

---
+++ precache-manifest.a6253c83f523d68bcb8ca6d418883038.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee2e447ddfa3422f531797e2be91cced",
+    "url": "/sd260final/index.html"
+  },
+  {
+    "revision": "0213a0339332b65984b1",
+    "url": "/sd260final/static/css/main.53a20964.chunk.css"
+  },
+  {
+    "revision": "f7b75fe91b2b22e51b61",
+    "url": "/sd260final/static/js/2.19f56990.chunk.js"
+  },
+  {
+    "revision": "efe80d506e138c343a03a1541511a2f3",
+    "url": "/sd260final/static/js/2.19f56990.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0213a0339332b65984b1",
+    "url": "/sd260final/static/js/main.dbdd1c96.chunk.js"
+  },
+  {
+    "revision": "0c4d21edb03246865585",
+    "url": "/sd260final/static/js/runtime-main.f0d6ec61.js"
+  }
+]);
